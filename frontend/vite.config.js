b5621d6099236cee06141eeafab360e16688b0ff
Chimbuchi <echimbuchi33@gmail.com>
2025-02-10 15:01:40 +0100

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  server: { port: 5173 },
   build: {
     rollupOptions: {
       external: ["react-router-dom"],
